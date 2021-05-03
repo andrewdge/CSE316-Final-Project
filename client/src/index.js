6f9f0f2f3f543e6e18b21be8b36c7883bd1c0c31
Andrew <andrewdge411@gmail.com>
2021-05-03 01:21:09 -0400

    edit merge

--- client/src/index.js
+++ client/src/index.js
@@ -15,7 +15,7 @@ const cache = new InMemoryCache({
 	typePolicies: {
 		Query: {
 			fields: {
-				getAllEntries: {
+				getAllMaps: {
 					merge(existing, incoming){
 						return incoming
 					}
