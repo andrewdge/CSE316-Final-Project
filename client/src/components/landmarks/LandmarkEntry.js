7631d899ddf2f4515456140bfe27d58bdee5f56a
Andrew <andrewdge411@gmail.com>
2021-05-16 20:58:16 -0400

    delete confirmation working

--- client/src/components/landmarks/LandmarkEntry.js
+++ client/src/components/landmarks/LandmarkEntry.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { WCard, WRow, WCol, WButton, WInput } from 'wt-frontend';
 import { Link, useHistory } from 'react-router-dom';
+import Delete from '../modals/Delete';
 
 const LandmarkEntry = (props) => {
 
@@ -9,6 +10,7 @@ const LandmarkEntry = (props) => {
     let name = props.entry.name;
     let location = props.entry.location;
     const [editingName, toggleNameEdit] = useState(false);
+    const [showDelete, setShowDelete]   = useState(false);
 
     const handleNameEdit = (e) => {
         toggleNameEdit(false);
@@ -21,38 +23,45 @@ const LandmarkEntry = (props) => {
         props.deleteLandmark(props.entry);
     }
 
+    const handleShowDelete = () => {
+        setShowDelete(!showDelete);
+    }
+
     return (
-        <WRow style={{ height: '40px', width: '100%', display: 'flex'}}>
-            {
-                props.canDelete ?
-                    <WButton size='small' style={{ color: 'red', backgroundColor: 'black' }} onClick={handleDeleteLandmark} shape='rounded' hoverAnimation='darken' clickAnimation='ripple-light'>
-                        <i className='material-icons'>clear</i>
-                    </WButton>
-                :
-                    <div style={{ marginLeft: '55px'}}></div>
-            }
-            
-            <div style={{ color: 'lightgrey', fontSize: '1.125rem', marginLeft: '30px', display: 'flex', alignItems: 'center' }}>
+        <>
+            <WRow style={{ height: '40px', width: '100%', display: 'flex'}}>
                 {
-                    editingName
-                        ? <WInput
-                            className='landmark-input' onBlur={handleNameEdit}
-                            autoFocus={true} defaultValue={name} type='text'
-                            wType="outlined" inputClass="landmark-input-class"
-                        />
-                        : <div className="table-text"
-                            onClick={() => toggleNameEdit(!editingName)}
-                        >{name}
-                        </div>
+                    props.canDelete ?
+                        <WButton size='small' style={{ color: 'red', backgroundColor: 'black' }} onClick={handleShowDelete} shape='rounded' hoverAnimation='darken' clickAnimation='ripple-light'>
+                            <i className='material-icons'>clear</i>
+                        </WButton>
+                    :
+                        <div style={{ marginLeft: '55px'}}></div>
                 }
-                 {props.entry.location && 
-                    <div style={{ marginLeft: '10px', marginRight: '10px' }}>
-                    {`  - `}
-                    </div>
-                } 
-                {props.entry.location}
-            </div>
-        </WRow>
+                
+                <div style={{ color: 'lightgrey', fontSize: '1.125rem', marginLeft: '30px', display: 'flex', alignItems: 'center' }}>
+                    {
+                        editingName
+                            ? <WInput
+                                className='landmark-input' onBlur={handleNameEdit}
+                                autoFocus={true} defaultValue={name} type='text'
+                                wType="outlined" inputClass="landmark-input-class"
+                            />
+                            : <div className="table-text"
+                                onClick={() => toggleNameEdit(!editingName)}
+                            >{name}
+                            </div>
+                    }
+                    {props.entry.location && 
+                        <div style={{ marginLeft: '10px', marginRight: '10px' }}>
+                        {`  - `}
+                        </div>
+                    } 
+                    {props.entry.location}
+                </div>
+            </WRow>
+            {showDelete && <Delete deleteFunction={handleDeleteLandmark} deleteName={props.entry.name} setShowDelete={setShowDelete} cancelFunction={handleShowDelete} />}
+        </>
     );
 };
 export default LandmarkEntry;
